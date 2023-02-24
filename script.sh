bf0fb759404ed19bc8fa44dcf737f7711add3a6a
dyego <dyego@gmail.com>
2023-02-24 18:03:54 -0300

    commit test

--- script.sh
+++ script.sh
@@ -14,8 +14,10 @@ JENKINS_USER_API_TOKEN=11bb4b85a8fe21b785aad90dcbfdff518d
 BASIC_AUTH=${JENKINS_USER}:$JENKINS_USER_API_TOKEN
 
 # Obtained from URL without any protocol
-JENKINS_ADDRESS="http://192.168.1.174:8081"
+JENKINS_ADDRESS="192.168.1.174:8081"
 
 
 # Pass parameters with --data option
-curl http://admin:11bb4b85a8fe21b785aad90dcbfdff518d@http://192.168.1.174:8081/job/test-scripted/buildWithParameters --data param1=123 --data param2="git-repo"
+#curl http://admin:11bb4b85a8fe21b785aad90dcbfdff518d@http://192.168.1.174:8081/job/test-scripted/buildWithParameters --data param1=123 --data param2="git-repo"
+curl http://${BASIC_AUTH}@${JENKINS_ADDRESS}/job/my_pipeline/buildWithParameters \
+  --data param1=123 --data param2="git-repo"
